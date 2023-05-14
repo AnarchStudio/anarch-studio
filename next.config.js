429bb8cd7bc325b2db52e5be07668a4969969b29
Suiko <suiko@riseup.net>
2023-05-14 12:37:53 -0700

    chore: add redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  redirects: async () => [
+    {
+      source: '/:path*',
+      has: [{type: "host", value: "astrasurge.com"}],
+      destination: 'https://www.astrasurge.com/:path*',
+      permanent: true,
+    }
+  ]
 }
 
 module.exports = nextConfig
